36bdc9322b71a71f53750b0c97b08e6dbc8d731f
LMezza <mezabalza18@gmail.com>
2024-10-25 11:20:17 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,32 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let generarexcusa = () => {
+  let articulo = ["Un", "Mi"];
+  let sujeto = ["perro", "gato", "hamster", "conejo"];
+  let hecho = ["mordio", "se comio", "orino", "vomito"];
+  let pertenencia = ["mi libro", "mi cuaderno", "mi mochila", "mi movil"];
+  let lugar = ["en la cama", "en la cocina", "en el comedor", "en el sofa"];
+
+  let articuloIndex = Math.floor(Math.random() * articulo.length);
+  let sujetoIndex = Math.floor(Math.random() * sujeto.length);
+  let hechoIndex = Math.floor(Math.random() * hecho.length);
+  let pertenenciaIndex = Math.floor(Math.random() * pertenencia.length);
+  let lugarIndex = Math.floor(Math.random() * lugar.length);
+
+  return (
+    articulo[articuloIndex] +
+    " " +
+    sujeto[sujetoIndex] +
+    " " +
+    hecho[hechoIndex] +
+    " " +
+    pertenencia[pertenenciaIndex] +
+    " " +
+    lugar[lugarIndex]
+  );
+};
 window.onload = function() {
-  //write your code here
+  document.querySelector("#excuse").innerHTML = generarexcusa();
   console.log("Hello Rigo from the console!");
 };
